209d41cbf0228a00b80a3f883a09d4b5c21db7d4
D-sai-rohith <108971395+D-sai-rohith@users.noreply.github.com>
2023-10-30 22:57:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-function convertToCelsius() {
+function toCelsius() {
     var fahrenheitInput = document.getElementById("fahrenheit");
     var fahrenheit = fahrenheitInput.value;
     var celsius = ((fahrenheit - 32) * 5/9).toFixed(2);
     var output = document.getElementById('output1');
-    output.innerHTML= fahrenheit + ' fahrenheit after converting into celsius is '+ celsius;
+    output.innerHTML= fahrenheit + ' °F = '+ celsius +" °C";
   }
 
-  function convertToFahrenheit() {
-    var fahrenheitInput = document.getElementById("fahrenheit");
+  function toFahrenheit() {
     var celsiusInput = document.getElementById("celsius");
     var celsius = parseFloat(celsiusInput.value);
     var fahrenheit = ((celsius * 9/5) + 32).toFixed(2);
     var output = document.getElementById('output2');
-    output.innerHTML= celsius + ' celsius after converting into fahrenheit is '+ fahrenheit;
+    output.innerHTML= celsius + ' °C = '+ fahrenheit +"°F";
     
   }
